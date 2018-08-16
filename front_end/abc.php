a40dfbe4b0bcbc9fc02c0f02360ce0785b0a895c
Munesh Yadav <31917609+S1MPLE001@users.noreply.github.com>
2018-08-16 11:01:45 +0530

    abc.php

--- front_end/abc.php
+++ front_end/abc.php
@@ -1,6 +1,12 @@
 <html>
-<body>
- <?php 
+<body bgcolor = teal>
+ <center>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br><br><br><br><br>
+ <h1><font color = "blue"><?php 
     $myfile = fopen("in.txt", "w") or die("Unable to open file!");
 $txt = $_GET["name"];;
 fwrite($myfile, $txt);
@@ -10,6 +16,9 @@ $txt = $_GET["name1"];;
 fwrite($myfile, $txt);
 fwrite($myfile, $nxt);
 fclose($myfile);
+echo exec("project.exe") ; 
 ?><br>
+</font></h1>
+</center>
  </body>
 </html>
